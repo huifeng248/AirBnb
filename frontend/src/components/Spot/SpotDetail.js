e49b082d95e4e717c4ff6c672c8f9ad444723e6a
Hui Feng <huifeng@MacBook-Pro.attlocal.net>
2022-10-06 14:50:26 -0700

    css homepage and spot by user

--- frontend/src/components/Spot/SpotDetail.js
+++ frontend/src/components/Spot/SpotDetail.js
@@ -2,27 +2,27 @@ import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import './Spot.css'
-import {getOneSpot, DeleteSpot} from '../../store/spot'
+import { getOneSpot, DeleteSpot } from '../../store/spot'
 import { useHistory } from 'react-router-dom';
 import ReviewBySpot from '../Review/ReviewBySpot'
 import ReviewFormModal from '../ReviewFormModal'
 
-function SpotDetail (){
-    const {id} = useParams()
+function SpotDetail() {
+    const { id } = useParams()
     const history = useHistory()
     const dispatch = useDispatch()
     const [isLoaded, setIsloaded] = useState(false)
     const spots = useSelector((state) => state.spots)
     const reviews = useSelector((state) => state.reviews)
     const spot = spots[+id]
-    const user = useSelector(state=> state.session.user)
+    const user = useSelector(state => state.session.user)
 
-    useEffect(()=> {
+    useEffect(() => {
         dispatch(getOneSpot(id))
-            .then(()=>setIsloaded(true))
-    },[dispatch, id, reviews])
+            .then(() => setIsloaded(true))
+    }, [dispatch, id, reviews])
 
-    
+
 
     // let reviewRating = "New"
     // console.log("hello", spot)
@@ -33,57 +33,68 @@ function SpotDetail (){
     // }
 
     return (
-        isLoaded&&
-            <div className='spot_detail_page_container'>
-                {/* <div key={`spot-details- ${spot.id}`}> */}
-                    <div>
-                    <h1 className='spot_detail_title'>{spot.name}</h1>
-                    <div className='spot_reviews spot_reviews_item'>
+        isLoaded &&
+        <div className='spot_detail_page_container'>
+            {/* <div key={`spot-details- ${spot.id}`}> */}
+            <div>
+                <h1 className='spot_detail_title'>{spot.name}</h1>
+                <div className='spot_reviews_item_container'>
+                    <div className='span_div'>
                         <i className="fa-solid fa-star spot_reviews_item"></i>
-                        <div>{spot.avgStatRating? (` ${spot.avgStatRating.toFixed(2)}`):"New"}</div>
-                        {/* <div>{reviewRating}</div> */}
-                        <div className='numbers_review spot_reviews_item'>{spot.numReviews}</div>
-                        <div>reviews</div>
-                        <div className='spot_reviews_item'>{`${spot.city},${spot.state},${spot.country}`}</div>
+                        <div className='review_char'>{spot.avgStatRating ? (` ${spot.avgStatRating.toFixed(2)}`) : "New"}</div>
                     </div>
+                    <div className='dot_div'> · </div>
+                    <div className='span_div'>
+                        <div className='review_char'>{spot.numReviews}</div>
+                        <div className='review_char'>reviews</div>
+                    </div>
+                    <div className='dot_div'> · </div>
+                    <div className='span_div'>
+                        <div className='spot_reviews_item'> Host by {spot.owner.firstName} {spot.owner.lastName}</div>
+                    </div>
+                    <div className='dot_div'> · </div>
+                    <div className='span_div'>
+                        <div className='spot_reviews_item'>{`${spot.city}, ${spot.state}, ${spot.country}`}</div>
+                    </div>
+                </div>
 
-                    <div className='main_body'>
-                
-                        <div className='spot_image_container'>
-                            <div className='spot_prevew_image_container'>
-                                <img className='spot_prevew_image' src={spot.previewImage}></img>
-                            </div> 
-                            <div className='spot_small_image_container'>
-                                {
-                                    spot.images&&
-                                    spot.images.map( (image, index) => (
-                                        <div  key={index} className='image_div'>
-                                            <img className='spot_small_image' src={image.url}></img>    
-                                        </div>
-                                        )
-                                    )
+                <div className='main_body'>
 
-                                }
-                            </div>
+                    <div className='spot_image_container'>
+                        <div className='spot_prevew_image_container'>
+                            <img className='spot_prevew_image' src={spot.previewImage}></img>
                         </div>
+                        <div className='spot_small_image_container'>
+                            {
+                                spot.images &&
+                                spot.images.map((image, index) => (
+                                    <div key={index} className='image_div'>
+                                        <img className='spot_small_image' src={image.url}></img>
+                                    </div>
+                                )
+                                )
 
-                        <div className='spot_description'>
-                            Description: {spot.description}
+                            }
                         </div>
                     </div>
-                    </div>
 
-                    <div>
-                        {user&&
-                        <ReviewFormModal action='Add Review'/>
-                    }
-                    </div>
-                    <div>
-                        <div className='review_section_container'>
-                        <ReviewBySpot />
+                    <div className='spot_description'>
+                        Description: {spot.description}
                     </div>
                 </div>
             </div>
+
+            <div>
+                {user &&
+                    <ReviewFormModal action='Add Review' />
+                }
+            </div>
+            <div>
+                <div className='review_section_container'>
+                    <ReviewBySpot />
+                </div>
+            </div>
+        </div >
 
     )
 
