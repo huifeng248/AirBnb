97f357f188368e291b494b2e6081cd25cecaf091
Hui Feng <huifeng@MacBook-Pro.attlocal.net>
2022-11-09 00:38:05 -0800

    css spot detail page

--- frontend/src/components/Review/ReviewBySpot.js
+++ frontend/src/components/Review/ReviewBySpot.js
@@ -33,25 +33,23 @@ function ReviewBySpot() {
             <div className='Review_by_spots_title'>
                 <div className='review_at_spot_detail_page'>
                     <div className='review_info_at_spot_detail_page'>
-                    <i className="fa-solid fa-star spot_reviews_item"></i>
-                    {spot.avgStatRating ? <h3 className='review_buffer_left'> {spot.avgStatRating}</h3>
-                        : <h3 className='review_buffer_left'>0.00</h3>
-                    }
+                        <i className="fa-solid fa-star spot_reviews_item"></i>
+                        {spot.avgStatRating ? <h3 className='review_buffer_left'> {spot.avgStatRating}</h3>
+                            : <h3 className='review_buffer_left'>0.00</h3>
+                        }
 
-                    <h3>·</h3>
+                        <h3>·</h3>
 
-                    {spot.numReviews ? <h3 className='review_buffer_right'>{spot.numReviews} Reviews</h3>
-                        : <h3 className='review_buffer_right'>New</h3>
-                    }
+                        {spot.numReviews ? <h3 className='review_buffer_right'>{spot.numReviews} Reviews</h3>
+                            : <h3 className='review_buffer_right'>New</h3>
+                        }
                     </div>
-                    {/* {user &&<div>
-                        <button className='add_review_button'>Add Review</button>
-                    </div>} */}
-                         {user &&
+                    {user &&
                         <ReviewFormModal action='Add Review' />
                     }
                 </div>
             </div>
+            <div className="review_detail_container">
             {
                 filteredReviews && filteredReviews.map(review => (
                     <div key={review.id}>
@@ -68,8 +66,9 @@ function ReviewBySpot() {
                         </div>
                     </div>
                 ))
-
+                
             }
+            </div>
         </div>
     )
 
